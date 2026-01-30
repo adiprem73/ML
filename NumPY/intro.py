ee1f895d3422ad88edded766f75cb1479b28c015
Aditya Prem <aditya.prem2003@gmail.com>
2026-01-30 21:06:28 +0530

    commit day 1

---
+++ NumPY/intro.py
@@ -0,0 +1,8 @@
+# lists are slow and numpy are fast 
+# faster to read because it uses less bytes of memory
+# benefits: SIMD vector processing & effective cache utilization
+# applications: mathematics( matlab replacement) & plotting & backend(pandas, connect 4, digital photography) & MACHINE LEARNING
+
+import numpy as np
+a = np.array([1,2,3])
+print("Hello")
